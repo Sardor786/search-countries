47b0d1cc7f724bb9d5777f794047566494a22e27
Sardor786 <shodmonovsardor766@gmail.com>
2023-02-18 16:51:10 +0500

    country card style

--- src/views/app-views/home/components/CountriesList.js
+++ src/views/app-views/home/components/CountriesList.js
@@ -4,22 +4,50 @@ import Card from "@mui/material/Card";
 export default function CountriesList({ countries = [], loading }) {
 	if (loading) return <h2 className="text-center">Loading...</h2>;
 
-	return countries.length > 0 ? (
-		countries.map((country) => {
-			return (
-				<Card key={country.code} variant="outlined">
-					<div>
-						<p>{country.emoji}</p>
-						<p>{country.name}</p>
-						<p>{country.capital}</p>
-						<p>{country.continent.name}</p>
-						<p>{country.phone}</p>
-						<p>{country.code}</p>
-					</div>
-				</Card>
-			);
-		})
-	) : (
-		<p className="text-center">Empty</p>
+	return (
+		<div className="countries_list">
+			{countries.length > 0 ? (
+				countries.map((country) => {
+					return (
+						<Card
+							className="countries_list-item"
+							key={country.code}
+							variant="outlined"
+						>
+							<div className="countries_list-content">
+								<div className="countries_list-header">
+									<div className="countries_list-emoji">
+										<span>{country.emoji}</span>
+									</div>
+									<div className="countries_list-names">
+										<p>{country.name}</p>
+										<p>{country.capital}</p>
+									</div>
+									<div className="countries_list-continent">
+										<p>{country.continent.name}</p>
+									</div>
+								</div>
+								<div className="countries_list-footer">
+									<p>📞 +{country.phone}</p>
+									<p>💱 {country.code}</p>
+									<div className="countries_list-languages">
+										<span>🗣</span>
+										{country.languages.map((lang) => {
+											return (
+												<span key={lang.name}>
+													{lang.native}
+												</span>
+											);
+										})}
+									</div>
+								</div>
+							</div>
+						</Card>
+					);
+				})
+			) : (
+				<p className="text-center">Empty</p>
+			)}
+		</div>
 	);
 }
